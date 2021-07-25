19d1d34843f58254d2b868f360e6577279479dc0
iamvidz <vim7127@gmail.com>
2021-07-25 16:49:51 +0530

    lab1_nltk

--- lab1_1.py
+++ lab1_1.py
@@ -39,3 +39,53 @@ print('\033[91m' + all_negative_tweets[random.randint(0,5000)])
 
 tweet = all_positive_tweets[2277]
 print(tweet)
+
+nltk.download('stopwords')  
+
+
+import re
+import string
+
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.tokenize import TweetTokenizer
+
+print('\033[92m' + tweet)
+print('\033[94m')
+
+tweet2 = re.sub(r'https?:\/\/.*[\r\n]*', '', tweet2)
+tweet2 = re.sub(r'#', '', tweet2)
+print(tweet2)
+
+
+print()
+print('\033[92m' + tweet2)
+print('\033[94m')
+
+tokenizer = TweetTokenizer(preserve_case=False)
+tweet_tokens = tokenizer.tokenize(tweet2)
+print()
+print('Tokenized string:')
+print(tweet_tokens)
+
+stopwords_english = stopwords.words('english')
+print('Stop words\n')
+print(stopwords_english)
+
+print('\nPunctuation\n')
+print(string.punctuation)
+
+print()
+print('\033[92m')
+print(tweet_tokens)
+print('\033[94m')
+
+tweets_clean = []
+for word in tweet_tokens: # Go through every word in your tokens list
+    if (word not in stopwords_english and # remove stopwords
+        word not in string.punctuation): # remove punctuation
+        tweets_clean.append(word)
+
+print('removed stop words and punctuation:')
+print(tweets_clean)
+
